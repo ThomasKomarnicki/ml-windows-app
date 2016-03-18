97c823becd3a512b6ef657f63d660f2dbeee3f5e
Thomas Komarnicki <komarnicki.thomas@gmail.com>
2016-03-17 21:41:41 -0400

    initial commit

---
+++ MediaLoaderWPF1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace MediaLoaderWPF1 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void addDirectoryButton_Click(object sender, RoutedEventArgs e) {
+            var dlg = new CommonOpenFileDialog();
+            dlg.Title = "Select Directory";
+            dlg.IsFolderPicker = true;
+            dlg.InitialDirectory = "C:/Users";
+
+            dlg.AddToMostRecentlyUsedList = false;
+            dlg.AllowNonFileSystemItems = false;
+            dlg.DefaultDirectory = "C:/Users";
+            dlg.EnsureFileExists = true;
+            dlg.EnsurePathExists = true;
+            dlg.EnsureReadOnly = false;
+            dlg.EnsureValidNames = true;
+            dlg.Multiselect = false;
+            dlg.ShowPlacesList = true;
+
+            if (dlg.ShowDialog() == CommonFileDialogResult.Ok) {
+                var folder = dlg.FileName;
+                // Do something with selected folder string
+            }
+        }
+    }
+}
